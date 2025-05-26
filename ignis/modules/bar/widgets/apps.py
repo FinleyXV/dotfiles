3869e744dffabb160adc3e4ff63ef38e92a362e5
Finley <118295065+FinleyXV@users.noreply.github.com>
2025-05-26 22:30:49 +0200

    Update apps.py

--- ignis/modules/bar/widgets/apps.py
+++ ignis/modules/bar/widgets/apps.py
@@ -34,18 +34,3 @@ class AppItem(Widget.Button):
         )
 
 
-class Apps(Widget.Box):
-    def __init__(self):
-        super().__init__(
-            child=applications.bind(
-                "pinned",
-                transform=lambda value: [AppItem(app) for app in value]
-                + [
-                    Widget.Button(
-                        child=Widget.Icon(image="start-here-symbolic", pixel_size=32),
-                        on_click=lambda x: app.toggle_window("ignis_LAUNCHER"),
-                        css_classes=["pinned-app", "unset"],
-                    )
-                ],
-            )
-        )
